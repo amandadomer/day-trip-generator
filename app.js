839c9474645eec32e1dcccc12b7754f6ec4121f4
Amanda Domer <amandadomer@Amandas-Air.attlocal.net>
2021-08-18 20:55:51 -0500

    added onew variables to test random generator

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 "use strict"
 
 let destinations = ["Los Angeles", "Chicago", "New York City", "Denver", "Minneapolis"];
-let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian", ""];
+let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian", "Japanese"];
 let transportation = ["car", "train", "plane", "bus", "hitchhike"];
 let entertainment = ["go to a concert", "go hiking", "go shopping", "go to a museum"];
 
 let destination = destinations [Math.floor(Math.random() * destinations.length)];
 console.log(destination);
+
+let restaurant = restaurants [Math.floor(Math.random() * restaurants.length)];
+console.log(restaurant);
+
+let transport = transportation [Math.floor(Math.random() * transportation.length)];
+console.log(transport);
+
+let entertainMe = entertainment [Math.floor(Math.random() * entertainment.length)];
+console.log(entertainMe);
