689a644a71d0474319b0bc849e8bb8113596c77a
Amanda Domer <amandadomer@Amandas-Air.attlocal.net>
2021-08-18 20:30:34 -0500

    added arrays with options

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+let destinations = ["Los Angeles", "Chicago", "New York City", "Denver"];
+let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian"];
+let transportation = ["car", "train", "plane", "bus"];
+let entertainment = ["concert", "hiking", "shopping", "going to a museum"];
