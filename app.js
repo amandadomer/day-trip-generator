8ec39d967333bf98ea9708e1298d54b2c21fd868
Amanda Domer <amandadomer@Amandas-Air.attlocal.net>
2021-08-18 21:05:55 -0500

    tested concatenated sentence

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 "use strict"
 
 let destinations = ["Los Angeles", "Chicago", "New York City", "Denver", "Minneapolis"];
-let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian", "Japanese"];
-let transportation = ["car", "train", "plane", "bus", "hitchhike"];
-let entertainment = ["go to a concert", "go hiking", "go shopping", "go to a museum"];
+let restaurants = ["Mexican", "fast food", "Chinese", "vegetarian", "Japanese"];
+let transportation = ["car", "train", "plane", "bus", "hitchhiking"];
+let entertainment = ["to a concert", "hiking", "shopping", "to a museum"];
 
 let destination = destinations [Math.floor(Math.random() * destinations.length)];
-console.log(destination);
 
 let restaurant = restaurants [Math.floor(Math.random() * restaurants.length)];
-console.log(restaurant);
 
 let transport = transportation [Math.floor(Math.random() * transportation.length)];
-console.log(transport);
 
 let entertainMe = entertainment [Math.floor(Math.random() * entertainment.length)];
-console.log(entertainMe);
+
+console.log("You will travel by " + transport + " to " + destination + ", where you will go " + entertainMe + " and eat at a " + restaurant + " restaurant." )
