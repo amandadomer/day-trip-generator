43fadd621ae2cf8ea43ede993b995bafe78de77c
Amanda Domer <amandadomer@Amandas-Air.attlocal.net>
2021-08-18 20:45:43 -0500

    added choices to each trip category and tested random number generator

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let destinations = ["Los Angeles", "Chicago", "New York City", "Denver"];
-let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian"];
-let transportation = ["car", "train", "plane", "bus"];
-let entertainment = ["concert", "hiking", "shopping", "going to a museum"];
+"use strict"
+
+let destinations = ["Los Angeles", "Chicago", "New York City", "Denver", "Minneapolis"];
+let restaurants = ["Mexican", "Italian", "Chinese", "Vegetarian", ""];
+let transportation = ["car", "train", "plane", "bus", "hitchhike"];
+let entertainment = ["go to a concert", "go hiking", "go shopping", "go to a museum"];
+
+let destination = destinations [Math.floor(Math.random() * destinations.length)];
+console.log(destination);
